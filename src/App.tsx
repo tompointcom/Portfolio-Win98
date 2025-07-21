6124b447b69ca27a7bb31e3b1629f13c9fddcd3f
tompointcom <tom.courcy@gmail.com>
2025-07-21 19:31:04 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My Portfolio</h1>
+        </header>
+      </div>
+      <footer className="App-footer">
+        <p>&copy; 2023 My Portfolio</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
